a5d9425f7686abe430f3469779c7bfb45c6a4d0f
whynot1597 <tahoesoccerdefender@yahoo.com>
2018-01-17 20:57:01 -0800

    Create HelloWorld and header outline

---
+++ homework00/HelloWorld.java
@@ -0,0 +1,17 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  HelloWorld.java
+ *  Purpose       :  Says "Hello world"; first program; provides a header template for Java programs
+ *  Author        :  Jeremy Goldberg
+ *  Date          :  01-17-2018
+ *  Description   :  See purpose (in this case; other files will have more details)
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+  }
+}
