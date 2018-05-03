ffae9fc339fe66d340d48958a8c9821c50d719fa
whynot1597 <tahoesoccerdefender@yahoo.com>
2018-05-02 19:47:30 -0700

    Update NegativeTargetValueException.java
    Added JavaDoc header

--- homework07/NegativeTargetValueException.java
+++ homework07/NegativeTargetValueException.java
@@ -1,3 +1,13 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  NegativeTargetValueException.java
+ * Purpose    :  @see Use with http://bjohnson.lmu.build/cmsi186web/homework07.html
+ * @author    :  Jeremy Goldberg
+ * Date       :  2018-05-03
+ * Description:  Handles error case when target value is negative
+ * Notes      :  Specific type of IllegalArgumentException
+ * Warnings   :  None
+ * Exceptions :  None
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 public class NegativeTargetValueException extends IllegalArgumentException {
   NegativeTargetValueException() {
     super("Target amount must be at least 0");
