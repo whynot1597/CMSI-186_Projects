6f9e8f13667fb6c226350fe8d0f6aa69629d54c8
whynot1597 <tahoesoccerdefender@yahoo.com>
2018-05-02 19:45:16 -0700

    Update DuplicateDenominationException.java
    Added JavaDoc header

--- homework07/DuplicateDenominationException.java
+++ homework07/DuplicateDenominationException.java
@@ -1,3 +1,13 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  DuplicateDenominationException.java
+ * Purpose    :  @see Use with http://bjohnson.lmu.build/cmsi186web/homework07.html
+ * @author    :  Jeremy Goldberg
+ * Date       :  2018-05-03
+ * Description:  Handles error case when coin values are repeated
+ * Notes      :  Specific type of IllegalArgumentException
+ * Warnings   :  None
+ * Exceptions :  None
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 public class DuplicateDenominationException extends IllegalArgumentException {
   DuplicateDenominationException() {
     super("Coin values cannot be repeated");
