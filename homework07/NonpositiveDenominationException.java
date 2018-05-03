2e013ee7abfe52c688a534679dae5fe46e35a394
whynot1597 <tahoesoccerdefender@yahoo.com>
2018-05-02 19:48:04 -0700

    Update NonpositiveDenominationException.java
    Added JavaDoc header

--- homework07/NonpositiveDenominationException.java
+++ homework07/NonpositiveDenominationException.java
@@ -1,3 +1,13 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  NonpositiveDenominationException.java
+ * Purpose    :  @see Use with http://bjohnson.lmu.build/cmsi186web/homework07.html
+ * @author    :  Jeremy Goldberg
+ * Date       :  2018-05-03
+ * Description:  Handles error case when coin values are 0 or negative
+ * Notes      :  Specific type of IllegalArgumentException
+ * Warnings   :  None
+ * Exceptions :  None
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 public class NonpositiveDenominationException extends IllegalArgumentException {
   NonpositiveDenominationException() {
     super("Coin values must be greater than 0");
